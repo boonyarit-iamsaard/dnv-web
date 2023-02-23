2baf240084f87ce7e88fb40407b996fcc92d4edb
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-02-24 02:32:41 +0700

    build: add default theme font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Noto Sans', ...defaultTheme.fontFamily.sans],
+        serif: ['Noto Serif', ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 };
